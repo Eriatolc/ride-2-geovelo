23586c65d3e68c695d1f7b720ec658f8820adf2a
Eriatolc <eriatolc@proton.me>
2024-04-24 16:23:47 +0200

    add config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,29 @@
 export default {
   strava: {
-    clientId: '118620',
-    clientSecret: 'ff155129530838279729adc57a7050faec5e6203',
-    clientAccessToken: '418d1a1e85d410c5715851f2d62a66ea7e23a603',
+    clientId: '',
+    clientSecret: '',
+    clientAccessToken: '',
     clientRefreshToken: ''
+  },
+  coros: {
+    url: '',
+    loginEndpoint: '',
+    activityQueryEndpoint: '',
+    activityDownloadEndpoint: '',
+    login: '',
+    password: ''
   },
   komoot: {
     // Komoot credentials
   },
   geovelo: {
     // Geovelo credentials
+    url: '',
+    apiKey: '',
+    source: '',
+    login: '',
+    password: '',
+    loginEndpoint: '',
+    importGPXEndpoint : ''
   }
 };
