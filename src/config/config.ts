01179e5bb376ac94083d8147e82507a3e0ae8dfc
Eriatolc <eriatolc@proton.me>
2024-05-03 22:56:35 +0200

    Use a .env file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,29 +1,25 @@
 export default {
   strava: {
-    clientId: '',
-    clientSecret: '',
-    clientAccessToken: '',
-    clientRefreshToken: ''
+    clientId: process.env.STRAVA_CLIENTID as string || '',
+    clientSecret: process.env.STRAVA_CLIENTSECRET as string || '',
+    clientAccessToken: process.env.STRAVA_CLIENTACCESSTOKEN as string || '',
+    clientRefreshToken: process.env.STRAVA_REFRESHTOKEN as string || ''
   },
   coros: {
-    url: '',
-    loginEndpoint: '',
-    activityQueryEndpoint: '',
-    activityDownloadEndpoint: '',
-    login: '',
-    password: ''
-  },
-  komoot: {
-    // Komoot credentials
+    url: process.env.COROS_URL || '',
+    loginEndpoint: process.env.COROS_LOGINENDPOINT || '',
+    activityQueryEndpoint: process.env.COROS_ACTIVITYQUERYENDPOINT || '',
+    activityDownloadEndpoint: process.env.COROS_ACTIVITYDOWNLOADENDPOINT || '',
+    login: process.env.COROS_LOGIN || '',
+    password: process.env.COROS_PASSWORD || ''
   },
   geovelo: {
-    // Geovelo credentials
-    url: '',
-    apiKey: '',
-    source: '',
-    login: '',
-    password: '',
-    loginEndpoint: '',
-    importGPXEndpoint : ''
+    url: process.env.GEOVELO_URL || '',
+    apiKey: process.env.GEOVELO_APIKEY || '',
+    source: process.env.GEOVELO_SOURCE || '',
+    login: process.env.GEOVELO_LOGIN || '',
+    password: process.env.GEOVELO_PASSWORD || '',
+    loginEndpoint: process.env.GEOVELO_LOGINENDPOINT || '',
+    importGPXEndpoint : process.env.GEOVELO_IMPORTGPXENDPOINT || ''
   }
 };
