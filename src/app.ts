b33ea4708f6f35684515825170a9e26d7f53b80c
Eriatolc <eriatolc@proton.me>
2024-05-04 00:09:24 +0200

    Fix if 0 activities have been found

--- src/app.ts
+++ src/app.ts
@@ -8,30 +8,32 @@ async function main () {
   console.log('Starting ride2geovelo application...')
   console.log('Config used: ', config)
   const today = DateTime.now()
-
+  console.log('Syncing rides for the date: ', today.toLocaleString({ month: 'long', day: 'numeric' }))
   const geovelo = new GeoveloService()
   await geovelo.authenticate()
   
   const corosService = new CorosService ()
   await corosService.authenticate()
+  console.log('Getting activities from Coros...')
   const corosActivities = await corosService.getRidingActivitiesByDate(today, today)
 
-  console.log('Getting activities from Coros...')
-  // @ts-ignore
-  const activitiesID = corosActivities.data.dataList.map(item => item.labelId)
-  console.log(`${activitiesID.length} activites to upload...`)
-  let corosGPXFile
   let count = 0
-  for await (const item of activitiesID) {
-    console.log('item loaded : ', item)
-    console.log('downloading GPX file...')
-    corosGPXFile = await corosService.downloadGPX(item)
-    console.log('GPX file downloaded. Pushing to Geovelo...')
-    await geovelo.pushGPX(corosGPXFile, item)
-    console.log('GPX file successfully pushed to Geovelo.')
-    count++
+  if (corosActivities && corosActivities.length && corosActivities.length > 0) {
+    // @ts-ignore
+    const activitiesID = corosActivities.data.dataList.map(item => item.labelId)
+    console.log(`${activitiesID.length} activites to upload...`)
+    let corosGPXFile
+    for await (const item of activitiesID) {
+      console.log('item loaded : ', item)
+      console.log('downloading GPX file...')
+      corosGPXFile = await corosService.downloadGPX(item)
+      console.log('GPX file downloaded. Pushing to Geovelo...')
+      await geovelo.pushGPX(corosGPXFile, item)
+      console.log('GPX file successfully pushed to Geovelo.')
+      count++
+    }
   }
-  console.log(`${count} riding activities have been pushed from Coros to Geovelo.`)
+  console.log(`${count} riding activities have been pushed from Coros to Geovelo for ${today.toLocaleString({ month: 'long', day: 'numeric' })}.`)
 }
 
 main()
